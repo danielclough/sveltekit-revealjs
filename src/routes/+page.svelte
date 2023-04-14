fe8330cb5a7ef77584bfaeb9e9c8313261d9b3ed
Daniel <danielclough@users.noreply.github.com>
2023-04-14 07:03:17 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from "svelte";
+    onMount(() => {
+        Reveal.initialize({
+            dependencies: [
+                { src: 'plugin/markdown/marked.js' },
+                { src: 'plugin/markdown/markdown.js' },
+                { src: 'plugin/notes/notes.js', async: true },
+                { src: 'plugin/highlight/highlight.js', async: true, callback: function () { hljs.initHighlightingOnLoad(); } }
+            ]
+        });
+    })
+
+    $: amazing = 0;
+</script>
+<div class="reveal">
+    <div class="slides">
+        <p>Revealed</p>
+        <section>
+            Svelte + RevealJS
+            {amazing}
+            <button on:click={()=>amazing++}>Reactive AF</button>
+        </section>
+        <section>Slide 2</section>
+    </div>
+</div>
+<div class="lower">
+    <p>Not Revealed</p>
+</div>
+
+<style>
+    .reveal {
+        height: 50%;
+    }
+    .lower {
+        height: 50%;
+        background-color: black;
+    }
+</style>
